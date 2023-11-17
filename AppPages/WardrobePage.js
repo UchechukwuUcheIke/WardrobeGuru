bf29a5c8c19a451eb0bc3634306fae7c4c45e2ca
yifan <yifan33@illinois.edu>
2023-11-16 23:59:06 -0600

    wardrobe add function with cloth item page

--- AppPages/WardrobePage.js
+++ AppPages/WardrobePage.js
@@ -1,9 +1,12 @@
 
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, FlatList } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, FlatList} from "react-native";
 import Icon from 'react-native-vector-icons/MaterialIcons'; // Assuming usage of MaterialIcons for icons
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import ClothingItemModal from "./ClothingItemPage";
+import { Modal, Button, ActivityIndicator} from 'react-native';
 
 import Data from "../Wardrobe_dataset.json";
 
@@ -18,6 +21,12 @@ export default function WardrobePage() {
     const [BottomImg, setBI] = useState(Data.Bottoms);
     const [AccessoriesImg, setAI] = useState(Data.Accessories);
     const [ShoesImg, setSI] = useState(Data.Shoes);
+
+    const [adding, setAdding] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+    const [currentItem, setCurrentItem] = useState({}); // The item currently being edited
+    
     
 
     function Wardrobe_Tops() {
@@ -292,46 +301,149 @@ export default function WardrobePage() {
         setSI(newShoes);
     }
 
+
+
+    function showModal() {
+        setIsModalVisible(true);
+      }
+      
+      function hideModal() {
+        setIsModalVisible(false);
+      }
+      
+      function handlePhotoOptionSelection(option) {
+        hideModal();
+        // Simulate a delay for photo selection or capture
+        setTimeout(() => {
+          AddNewItem();
+        }, 1000);
+      }
+
+    function AddNewItem() {
+        // Step 1: Simulate selecting a photo
+        const newItem = {
+          "id": Date.now().toString(), // Unique ID for the new item
+          "url": "https://store.nytimes.com/cdn/shop/products/TruthHoodie-WhiteFront_1024x1024.jpg?v=1571439084",
+          "category": "tops",
+          "selected": false,
+          "nickname": "New Hoodie",
+          "formality": "casual",
+          "warmth": "warm",
+          "timesWorn": 0,
+          "dateAdded": new Date().toLocaleDateString()
+        };
+      
+        // Step 2: Show loading icon
+        setAdding(true);
+      
+        setTimeout(() => {
+          // Step 3: Add the new item to the state
+          setAdding(false);
+          setCurrentItem(newItem); // Set the new item as the current item
+          setIsEditModalVisible(true); // Show the edit modal
+        }, 2000); // Simulate loading for 2 seconds
+      }
+
+    // Define a function to handle saving the edited item
+    const handleSaveItem = (editedItem) => {
+        setTI(prevItems => [editedItem, ...prevItems]);
+        setIsEditModalVisible(false); // Hide the edit modal
+    };     
+
     return (
         <View style={styles.container}>
-            {Select && <View style={styles.header}>
-                <TouchableOpacity style={styles.selectButton} onPress={PressSB}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isModalVisible}
+            onRequestClose={hideModal}
+          >
+            <View style={styles.modalView}>
+              <Button title="Take Photo" onPress={() => handlePhotoOptionSelection('take')} />
+              <Button title="Upload Photo" onPress={() => handlePhotoOptionSelection('upload')} />
+              <Button title="Cancel" onPress={hideModal} />
+            </View>
+          </Modal>
+      
+          {adding ? (
+            // Show loading indicator when adding a new item
+            <View style={{ ...styles.header, justifyContent: 'center' }}>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+          ) : (
+            <>
+              {Select && (
+                <View style={styles.header}>
+                  <TouchableOpacity style={styles.selectButton} onPress={PressSB}>
                     <Text style={styles.selectButtonText}>Select</Text>
-                </TouchableOpacity>
-                <Text style={styles.headerText}>Wardrobe</Text>
-                <TouchableOpacity>
+                  </TouchableOpacity>
+                  <Text style={styles.headerText}>Wardrobe</Text>
+                  <TouchableOpacity onPress={showModal}>
                     <Icon name="add" size={30} color="#000" />
-                </TouchableOpacity>
-            </View>}
-            {Select && <Tab.Navigator>
-                <Tab.Screen name="Tops" component={Wardrobe_Tops} />
-                <Tab.Screen name="Bottoms" component={Wardrobe_Bottoms} />
-                <Tab.Screen name="Accessories" component={Wardrobe_Accessories} />
-                <Tab.Screen name="Shoes" component={Wardrobe_Shoes} />
-            </Tab.Navigator>}
-            {!Select && <View style={styles.header}>
-                <TouchableOpacity style={styles.selectButton} onPress={PressSB}>
+                  </TouchableOpacity>
+                </View>
+              )}
+              {Select && (
+                <Tab.Navigator>
+                  <Tab.Screen name="Tops" component={Wardrobe_Tops} />
+                  <Tab.Screen name="Bottoms" component={Wardrobe_Bottoms} />
+                  <Tab.Screen name="Accessories" component={Wardrobe_Accessories} />
+                  <Tab.Screen name="Shoes" component={Wardrobe_Shoes} />
+                </Tab.Navigator>
+              )}
+              {!Select && (
+                <View style={styles.header}>
+                  <TouchableOpacity style={styles.selectButton} onPress={PressSB}>
                     <Text style={styles.selectButtonText}>Cancel</Text>
-                </TouchableOpacity>
-                <Text style={styles.headerText}>Wardrobe</Text>
-                <TouchableOpacity onPress={deleteImg}>
+                  </TouchableOpacity>
+                  <Text style={styles.headerText}>Wardrobe</Text>
+                  <TouchableOpacity onPress={deleteImg}>
                     <Icon name="delete" size={30} color="#000" />
-                </TouchableOpacity>
-            </View>}
-            {!Select && <Tab.Navigator>
-                <Tab.Screen name="Tops" component={Wardrobe_Tops_Select} />
-                <Tab.Screen name="Bottoms" component={Wardrobe_Bottoms_Select} />
-                <Tab.Screen name="Accessories" component={Wardrobe_Accessories_Select} />
-                <Tab.Screen name="Shoes" component={Wardrobe_Shoes_Select} />
-            </Tab.Navigator>}
+                  </TouchableOpacity>
+                </View>
+              )}
+              {!Select && (
+                <Tab.Navigator>
+                  <Tab.Screen name="Tops" component={Wardrobe_Tops_Select} />
+                  <Tab.Screen name="Bottoms" component={Wardrobe_Bottoms_Select} />
+                  <Tab.Screen name="Accessories" component={Wardrobe_Accessories_Select} />
+                  <Tab.Screen name="Shoes" component={Wardrobe_Shoes_Select} />
+                </Tab.Navigator>
+              )}
+            </>
+          )}
+
+            <ClothingItemModal
+                visible={isEditModalVisible}
+                onClose={() => setIsEditModalVisible(false)}
+                onSave={handleSaveItem}
+                item={currentItem}
+            />
         </View>
-    );
+      );
+      
+      
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+      },
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
