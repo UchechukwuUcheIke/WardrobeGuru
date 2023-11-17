09d9abed085526ab0b95fde763185f9b6fb9389f
Uche <uuchei2@illinois.edu>
2023-11-17 10:56:03 -0600

    Added OutfitDisplay Component, fixed ESLint Errors in Icon Button and Text Button

---
+++ Component/OutfitDisplay.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { StyleSheet, Image, View, ViewPropTypes } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function OutfitDisplay({ style, hatImgSrc, shirtImgSrc, pantsImgSrc, shoesImgSrc }) {
+
+    return (
+        <View style={style}>
+            <Image
+                style={styles.image} 
+                source={{
+                    uri: hatImgSrc,
+                }}
+            />
+            <Image
+                style={styles.image} 
+                source={{
+                    uri: shirtImgSrc,
+                }}
+            />
+            <Image
+                style={styles.image} 
+                source={{
+                    uri: pantsImgSrc,
+                }}
+            />
+            <Image
+                style={styles.image} 
+                source={{
+                    uri: shoesImgSrc,
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        height: "20%",
+        marginBottom: "1%"
+    },
+});
+
+OutfitDisplay.propTypes = {
+
+style: ViewPropTypes.style,
+hatImgSrc: PropTypes.string,
+shirtImgSrc: PropTypes.string,
+pantsImgSrc: PropTypes.string,
+shoesImgSrc: PropTypes.string
+
+};
+
+OutfitDisplay.defaultProps = {
+    style: {},
+    hatImgSrc: " ",
+    shirtImgSrc: " ",
+    pantsImgSrc: " ",
+    shoesImgSrc: " "
+}
